9f41468e59353e413bc7c232e3346f641d90cfe1
Subham Misra <69148853+subham-misra@users.noreply.github.com>
2022-02-20 23:23:09 +0530

    Add files via upload

---
+++ Javascript/local&session.js
@@ -0,0 +1,15 @@
+console.log("storage");
+/*localStorage.setItem('name','subham');
+localStorage.setItem('name2','sumit');
+let val=localStorage.getItem('name');
+//localStorage.removeItem('name');
+//localStorage.clear();
+
+let ar=["aba","jhf","try"];
+
+localStorage.setItem('array',JSON.stringify(ar));
+val=localStorage.getItem('array');
+//console.log(val);
+
+sessionStorage.setItem('name','subham');
+sessionStorage.setItem('name2','sumit');*/
